9730470c106eff64d0217a75a0c2aa2c5e5431b4
Volga Daniluk <olgadaniluk.i@gmail.com>
2021-08-19 10:53:57 +0300

    added template of servlets and filters

---
+++ src/main/java/com/orangeteam/Control_List/Servlets/UserActivitiesServlet.java
@@ -0,0 +1,25 @@
+package com.orangeteam.Control_List.Servlets;
+
+import com.orangeteam.Control_List.dao.ActivityDAOImpl;
+import com.orangeteam.Control_List.model.Activity;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/user_activities")
+public class UserActivitiesServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<Activity> activityList = ActivityDAOImpl.getAll();
+        req.setAttribute(activityList);
+
+        getServletContext().getRequestDispatcher("/user_activities.jsp").forward(req, resp);
+    }
+
+}
